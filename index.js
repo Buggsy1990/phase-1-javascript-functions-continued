4949a681059c9ec8f23c87898543e14d142f1017
Christina <buggsy1106@gmail.com>
2022-03-03 16:41:13 -0600

    commit tree e9883cec6dee145a6b347a78b7c75d22907e63c3

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturday('bathe my dog');
+
+function mondayWork(task='go to the office'){
+    return `This Monday, I will ${task}.`
+}
+mondayWork(`work from home`)
+
+function wrapAdjective(result='*'){
+        return function inner(origin='special'){
+            return `You are ${result}${origin}${result}!`
+        }
+}
+wrapAdjective('||')('a dedicated programmer!');
